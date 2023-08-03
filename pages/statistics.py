09b0a28d3181331b125d1a0e26a9e7f2338d32fd
Aiara Lobo Gomes <aiara.lobogomes@maastro.nl>
2023-08-03 16:55:00 +0200

    Template for statistics page

--- pages/statistics.py
+++ pages/statistics.py
@@ -7,13 +7,10 @@ import os
 import logging
 
 import pandas as pd
+import plotly.express as px
 
 from dash import dcc
 from dash import html
-from dash import dash_table
-from dash.dependencies import Input
-from dash.dependencies import Output
-from dash.dependencies import State
 
 from app import app
 
@@ -24,12 +21,55 @@ logging.basicConfig(
     datefmt='%d-%b-%y %H:%M:%S'
 )
 
+df = pd.read_csv('data/lung_reduced.csv')
+df['date_of_diagnosis'] = pd.to_datetime(df['date_of_diagnosis'])
+df['date_of_fu'] = pd.to_datetime(df['date_of_fu'])
+df['days'] = df['date_of_fu'] - df['date_of_diagnosis']
+
 
 # ------------------------------------------------------------------------------
 # Statistics page layout
 # ------------------------------------------------------------------------------
 layout = html.Div([
     html.H1('Statistics'),
-    html.Hr()
+    html.Hr(),
+    html.P(),
+    html.Div([
+        dcc.Graph(
+            id='figure1',
+            figure = px.histogram(df, x='centre')
+        ),
+    ],
+    style={
+        'width': '49%', 'display': 'inline-block', 'vertical-align': 'middle'
+    }),
+    html.Div([
+        dcc.Graph(
+            id='figure1',
+            figure=px.histogram(df, x='centre', color='stage')
+        ),
+    ],
+    style={
+        'width': '49%', 'display': 'inline-block', 'vertical-align': 'middle'
+    }),
+    html.Div([
+        dcc.Graph(
+            id='figure1',
+            figure=px.histogram(df, x='centre', color='vital_status')
+        ),
+    ],
+    style={
+        'width': '49%', 'display': 'inline-block', 'vertical-align': 'middle'
+    }),
+    html.Div([
+        dcc.Graph(
+            id='figure1',
+            figure=px.histogram(df, x='days', color='centre')
+        ),
+    ],
+    style={
+        'width': '49%', 'display': 'inline-block', 'vertical-align': 'middle'
+    }),
+    html.P()
 ])
 
