4e75eb145c52e4011c7ca53dbb8dcb6928de35fd
Aiara Lobo Gomes <aiara.lobogomes@maastro.nl>
2023-08-04 10:41:20 +0200

    Plots from aggregated statistics

--- pages/statistics.py
+++ pages/statistics.py
@@ -4,8 +4,10 @@
 TNM statistics
 """
 import os
+import re
 import logging
 
+import numpy as np
 import pandas as pd
 import plotly.express as px
 
@@ -21,10 +23,32 @@ logging.basicConfig(
     datefmt='%d-%b-%y %H:%M:%S'
 )
 
+
+# ------------------------------------------------------------------------------
+# Get data
+# ------------------------------------------------------------------------------
+# Read data
 df = pd.read_csv('data/lung_reduced.csv')
 df['date_of_diagnosis'] = pd.to_datetime(df['date_of_diagnosis'])
 df['date_of_fu'] = pd.to_datetime(df['date_of_fu'])
-df['days'] = df['date_of_fu'] - df['date_of_diagnosis']
+df['days'] = df.apply(
+    lambda x: (x['date_of_fu'] - x['date_of_diagnosis']).days, axis=1
+)
+
+# Patients per centre
+dfg1 = df.groupby('centre')['id'].nunique().reset_index()
+
+# Patients per centre per stage
+dfg2 = df.groupby(['centre', 'stage'])['id'].nunique().reset_index()
+
+# Patients per centre per vital status
+dfg3 = df.groupby(['centre', 'vital_status'])['id'].nunique().reset_index()
+
+# Interval between diagnosis and follow up
+bins = [0, 3*31, 6*31, 9*31, 12*31, 15*31, 18*31, 21*31, 24*30, np.Inf]
+df['days'] = pd.cut(df['days'], bins)
+dfg4 = df.groupby(['centre', 'days'])['id'].nunique().reset_index()
+dfg4['days'] = dfg4['days'].apply(lambda x: x.right)
 
 
 # ------------------------------------------------------------------------------
@@ -36,8 +60,7 @@ layout = html.Div([
     html.P(),
     html.Div([
         dcc.Graph(
-            id='figure1',
-            figure = px.histogram(df, x='centre')
+            figure = px.bar(dfg1, x='centre', y='id')
         ),
     ],
     style={
@@ -45,8 +68,7 @@ layout = html.Div([
     }),
     html.Div([
         dcc.Graph(
-            id='figure1',
-            figure=px.histogram(df, x='centre', color='stage')
+            figure=px.bar(dfg2, x='centre', y='id', color='stage')
         ),
     ],
     style={
@@ -54,8 +76,7 @@ layout = html.Div([
     }),
     html.Div([
         dcc.Graph(
-            id='figure1',
-            figure=px.histogram(df, x='centre', color='vital_status')
+            figure=px.bar(dfg3, x='centre', y='id', color='vital_status')
         ),
     ],
     style={
@@ -63,8 +84,7 @@ layout = html.Div([
     }),
     html.Div([
         dcc.Graph(
-            id='figure1',
-            figure=px.histogram(df, x='days', color='centre')
+            figure=px.bar(dfg4, x='days', y='id', color='centre')
         ),
     ],
     style={
